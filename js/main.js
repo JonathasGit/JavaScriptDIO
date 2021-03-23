c7ac282b20d8ae3089a99f64bdfa67fb677a0a00
Jonathas <jonathas-25@hotmail.com>
2021-03-23 00:46:26 -0300

    #2 Comentários em JS
    Como comentar o código em JS.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
-var nome = "Jonathas ";
+var nome = "Jonathas "; //variaveis 
 var idade = "25"
 alert(" Bem vindo " + nome + "de " + idade + " anos," + " ao meu primeiro JavaScript 22/03/2021 - Momento de fazer historia");
+//alert(nome +"Jonathas" + "tem" + idade); Aqui segundo alerta
