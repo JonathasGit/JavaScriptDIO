14fbfdff9d4ec4e1c5be7bdf30515f33e97f29ac
Jonathas <jonathas-25@hotmail.com>
2021-03-23 12:41:30 -0300

    #2 Alterações novas
    Só nova variavel

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var nome = "Jonathas "; //variaveis 
 var idade = "25"
+var idade2 = "10"
 alert(" Bem vindo " + nome + "de " + idade + " anos," + " ao meu primeiro JavaScript 22/03/2021 - Momento de fazer historia");
 //alert(nome +"Jonathas" + "tem" + idade); Aqui segundo alerta
