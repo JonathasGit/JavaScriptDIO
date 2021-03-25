b7467e8bfd118c711fd9546d0d82070c245aa11e
Jonathas <jonathas-25@hotmail.com>
2021-03-24 23:36:57 -0300

    #2 Subindo commit
    Subir commit

--- js/main.js
+++ js/main.js
@@ -3,3 +3,4 @@ var idade = "25"
 var idade2 = "10"
 alert(" Bem vindo " + nome + "de " + idade + " anos," + " ao meu primeiro JavaScript 22/03/2021 - Momento de fazer historia");
 //alert(nome +"Jonathas" + "tem" + idade); Aqui segundo alerta
+// Continuação da aula 
