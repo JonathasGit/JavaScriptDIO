86ff991c385bcf395ce06ddc10bf8344ca3019d3
Jonathas <jonathas-25@hotmail.com>
2021-03-25 00:20:45 -0300

    #2 Console
    Mostrar mensagens no console.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var nome = "Jonathas "; //variaveis 
 var idade = "25"
 var idade2 = "10"
+var frase = "Japão é o melhor time do mundo"
 alert(" Bem vindo " + nome + "de " + idade + " anos," + " ao meu primeiro JavaScript 22/03/2021 - Momento de fazer historia");
 //alert(nome +"Jonathas" + "tem" + idade); Aqui segundo alerta
 // Continuação da aula 
+console.log(nome); // Vai mostrar só no navegador.
+console.log(idade2);
+console.log(frase.replace("Japão", "Brasil")); // Troca as palavras
