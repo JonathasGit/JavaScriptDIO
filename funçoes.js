e5af5e3d08bfb4e54a753821cc3f12688afd2969
Jonathas <jonathas-25@hotmail.com>
2021-04-01 23:12:17 -0300

    #5 Var global e local
    Função validar, validar a idade e retornar ela mesma, somar n1 + n2, diferença de variavel.

---
+++ funçoes.js
@@ -0,0 +1,28 @@
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function validaIdade(idade){
+var validar;
+if (idade >=18){
+    validar = true
+} else{
+    validar = false
+}
+return validar;
+}
+
+var idade = prompt("Qual a sua idade");
+console.log(validaIdade(idade));
+
+
+
+
+
+
+/*function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}*/
+
+alert(soma(5,10));
+//alert(setReplace("Vai Japão","Japão","Brasil")); Troca o nome vai japao por vai brasil 
